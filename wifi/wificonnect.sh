6368c1b7c7ee7c294cee91c764ae3669adc73548
palash <palash21gupta@gmail.com>
2024-02-01 10:13:01 +0530

    dmenu wifi

--- wifi/wificonnect.sh
+++ wifi/wificonnect.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
-declare -A wifisuffixes=( ["H"]="-C_HOSTEL" ["N"]="-NAB" ["B"]="" ["L"]="-WIFI" ["D"]="-DH" ["C"]="-CH")
-echo "Enter location [H: DH3, C: Campus, N: NAB, L: Lecture Theatre, D: D-Mess, C: C-Mess"
-read wifilocation
+wifilocation=$(cat /home/reiter/scripts/wifi/ssids | dmenu)
 
-if [[ -v wifisuffixes["${wifilocation^^}"] ]]; then
-    sudo nmcli dev wifi connect "BPGC${wifisuffixes["${wifilocation^^}"]}"
-    chromium https://campnet.bits-goa.ac.in:8090/
-else
-    echo "incorrect location"
-fi
+sudo nmcli dev wifi connect $wifilocation
+chromium https://campnet.bits-goa.ac.in:8090/
